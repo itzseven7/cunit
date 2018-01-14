2ff9ebe62248d54dc13c2d59775ba6c6fdd3a92f
Romain <itzseven@macbook-pro-de-romain.local>
2018-01-14 14:53:38 +0100

    enhance(assert) : print expression

--- src/ctassert.c
+++ src/ctassert.c
@@ -12,12 +12,12 @@
 #include "ctassert.h"
 #include "_ctest.h"
 
-void _cfail(const ctest_t *test, const char *assertion, const char *filename, unsigned long lineNumber, const char * format, ...) {
+void _cfail(const ctest_t *test, const char *expr1, const char *assertion, const char *expr2, const char *filename, unsigned long lineNumber, const char * format, ...) {
     
     ((ctest_int_t *)test->_internal)->failures++;
     
-    char buffer[strlen(assertion) + strlen(filename) + strlen (format) + 50];
-    sprintf(buffer, "%s failed at line %lu of file %s.\n%s", assertion, lineNumber, filename, format);
+    char buffer[strlen(filename) + strlen(expr1) + strlen(assertion) + strlen(expr2) + strlen (format) + 50];
+    sprintf(buffer, "Assertion failed at line %lu of file %s => %s %s %s\n%s", lineNumber, filename, expr1, assertion, expr2, format);
     va_list argptr;
     va_start(argptr, format);
     vfprintf(stderr, buffer, argptr);
